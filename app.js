95fe0ffa7c513c87391a5db34d083747ec5dad9f
JoaquinNinaCH <102237192+JoaquinNinaCH@users.noreply.github.com>
2023-04-12 08:41:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+
+app.post('/matriculas', (req, res) => {
+    const usuario = req.body.usuario;
+    const contrasena = req.body.contrasena;
+
+   
+    if (usuario === 'Joaquin' && contrasena === '123456789') {
+        res.render('matriculas');
+    } else {
+        
+        res.render('login', { error: 'Usuario o contraseña incorrectos' });
+    }
+});
+
+
+app.post('/confirmacion', (req, res) => {
+    const curso = req.body.curso;
+    const medioPago = req.body.medioPago;
+    const niveles = req.body.niveles;
+    
+    res.render('confirmacion', { curso, medioPago, niveles });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
